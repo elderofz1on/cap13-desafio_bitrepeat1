cdf3bcf912bb334660559b95ce100304c62a2229
Wagner Rodrigues <wprodriguesbr@gmail.com>
2022-06-24 18:15:09 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,23 @@
+/**
+ * Pontos de atenção: BLOCOS TEMPORÁRIOS
+ * 
+ * Nós deixamos disponível todos os blocos necessários para a programação completa da página 196, porém durante a jornada de programação do início (pág. 180) até o final na página 197, blocos extras são necessários, principalmente para testes de validação.
+ * 
+ * Quando você não encontrar algum destes blocos, você poderá adicioná-los e depois de utilizado podem ser removidos.
+ * 
+ * Ex.:
+ * 
+ * - na pág. 180, utilizamos o bloco NO BOTÃO A PRESSIONADO temporariamente, para testar alguma funcionalidade.
+ * 
+ * Ele não faz parte da programação final e não está listado neste conjunto de blocos.
+ */
+function criar_sequencia () {
+	
+}
+function ativar_leds (seta: string) {
+	
+}
+game.setScore(0)
+basic.forever(function () {
+	
+})
